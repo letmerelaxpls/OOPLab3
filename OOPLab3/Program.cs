88e74651a14174ccce458118a74e4edf8f657952
Georgiy <larsubivec@gmail.com>
2022-10-04 21:05:07 +0400

    second commit

--- OOPLab3/Program.cs
+++ OOPLab3/Program.cs
@@ -6,10 +6,45 @@ using System.Threading.Tasks;
 
 namespace OOPLab3
 {
+    class Rectangle
+    {
+        private double side1, side2;
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        public Rectangle() { }
+
+        double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+        double PerimeterCalculator()
+        {
+            return (side1 + side2) * 2;
+        }
+        public double Area { get { return this.AreaCalculator(); } }
+        public double Perimeter { get { return this.PerimeterCalculator(); } }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the length of the rectangle");
+            string x = Console.ReadLine();
+            double side1 = Int32.Parse(x);
+
+            Console.WriteLine("enter the width of the rectangle");
+            string y = Console.ReadLine();
+            double side2 = Int32.Parse(y);
+
+            Rectangle rect = new Rectangle(side1, side2);
+
+            Console.WriteLine("area = {0}", rect.Area);
+            Console.WriteLine("Perimeter = {0}", rect.Perimeter);
+
         }
     }
 }
